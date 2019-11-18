50118370b2f505daae1a939819d2fd0ba46618e6
Paul <paul.ponce@tecsup.edu.pe>
2019-11-18 15:35:59 -0500

    ExamenPersona

---
+++ ExamenPersona/TodoItemPersona.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExamenPersona
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodoItemPersona : ContentPage
+    {
+        public TodoItemPersona()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItemPersona)BindingContext;
+            await App.Database.SaveItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItemPersona)BindingContext;
+            await App.Database.DeleteItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+        async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+
+
+    }
+}
---
+++ ExamenPersona/TodoPersona.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExamenPersona
+{
+    class TodoPersona
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string Dni { get; set; }
+        public bool Done { get; set; }
+    }
+
+}
+
